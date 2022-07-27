6f3f3894e627ab6265582e5d259e3bd606f459bc
Daniel Westphal <dwestphal71@gmail.com>
2022-07-27 00:30:39 -0700

    home page core

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import "../styles/App.css"
 import "../styles/Home.css"
 import "../styles/Circles.css"
+import GitHub from '../images/github.svg';
+import LinkedIn from '../images/linkedin.svg';
+import Mail from '../images/mail.svg'
 import React from "react"
 import NavBar from "../components/NavBar"
 
@@ -16,45 +19,51 @@ export default function Home(props) {
                 </ul>
             </div>
 
-            <NavBar selected = "Home"/>
-           
+            <NavBar selected="Home" />
+
 
 
             <div className="about">
                 <h1 id="name">Hi, I'm <em className="accent">Daniel</em></h1>
                 <h2 id="title">Software Engineer & Designer </h2>
                 <p id="bio">
-                    I'm a rising senior at <b>Dartmouth College</b> double majoring in
-                    <b> Computer Science Modified with Human Centered Design</b> and <b>Physics</b>.
-                    I'm interested in how accessible design can help to alleviate societal issues.
-                </p>
+                    I'm interested in how universal design can help to alleviate societal issues.
+                    <br />
+                    <br />
+                    Currently looking for <b>full time software engineering positions for 2023!</b>
+                    <br />
+                    <br />
+                    Contact Me:
 
-                <button className="info" onClick={(e) => { /*window.open(Resume, "_blank")*/ }}>
-                    Download My Resume
-                </button>
-                <button className="info" onClick={() => window.location = 'mailto:daniel.e.westphal.23@dartmouth.edu'}>
-                    Contact Me!
-                </button>
+                </p>
             </div>
 
+            <div>
+                <div style={{ height: '4vw' }}>
+
+                    <a href="https://github.com/danielwestphal" target="_blank" rel="noopener noreferrer">
+                        <img src={GitHub} alt="GitHub Logo" />
+                        @danielwestphal
+                    </a>
+                </div>
+                <br />
+                <div>
+                    <a href="https://www.linkedin.com/in/danielwestphal" target="_blank" rel="noopener noreferrer">
+                        <img src={LinkedIn} alt="LinkedIn Logo" />
+                        Daniel Westphal
+                    </a>
+                </div>
+                <br />
+                <div>
+                    <a href="mailto:dew.23@dartmouth.edu" target="_top">
+                        <img src={Mail} alt="Envelope" />
+                        dew.23@dartmouth.edu
+                    </a>
+                </div>
+
+            </div>
 
 
         </div>
     );
 }
-
-/* 
-
-import GitHub from '../images/github.svg';
-import LinkedIn from '../images/linkedin.svg';
-<div className="links">
-                
-                <a href="https://github.com/danielwestphal" target="_blank" rel="noopener noreferrer">
-                    <img src={GitHub} alt="GitHub Logo" />
-                    </a>
-                
-                <a href="https://www.linkedin.com/in/danielwestphal" target="_blank" rel="noopener noreferrer">
-                    <img src={LinkedIn} alt="LinkedIn Logo" />
-                </a>
-            </div>
-            */
