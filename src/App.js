78a30645951e45660b1758469ae213937f60436c
Daniel Westphal <dwestphal71@gmail.com>
2022-07-26 22:00:37 -0700

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route }
-  from 'react-router-dom';
-import Home from './Home';
-import Media from './Media'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<Media />} />
+        {/*<Route path='/about' element={<About />} />*/}
+        <Route path='/portfolio' element={<Portfolio />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/App.css'
+import '../styles/Portfolio.css'
+
+export default function Portfolio(props) {
+    return (
+        <div className="Portfolio">
+            <h1 className='dark'> My Portfolio</h1>
+        </div>
+        
+    );
+};
