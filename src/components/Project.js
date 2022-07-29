1a30b3449a87140b6d2be233b37ba9c0f61d3c9f
Daniel Westphal <dwestphal71@gmail.com>
2022-07-29 01:26:18 -0700

    hover state

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,9 +4,27 @@ import '../styles/Project.css'
 import Ajax from "../images/ajax.png"
 
 export default function Project(props) {
+    const [info, setInfo] = React.useState(false);
+
     return (
-        <div className="project">
-            <img className="project" src={Ajax} alt="project!" />
+        <div className="project" onMouseEnter={() => setInfo(true)} onMouseLeave={() => setInfo(false)}>
+            <img className={info ? "project blur" : "project"} src={Ajax} alt="project!" />
+            {info &&
+                (<div className='info'>
+                    <div className='information'>
+                        <h5>
+                            Ajax
+                        </h5>
+                        <h6>
+                            Fall 2021
+                        </h6>
+                        <br></br>
+                        <p className='description'>
+                            Custom ray-tracing algorithm for dielectric, matte, and reflective surfaces.
+                        </p>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
