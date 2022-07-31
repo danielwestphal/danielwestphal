733da1f84fc1ad2233910ac30b51523c02907da7
Daniel Westphal <dwestphal71@gmail.com>
2022-07-31 16:54:27 -0700

    footprints

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "../styles/App.css"
 import "../styles/NavBar.css"
+import logo from "../images/logo.svg"
 //import Resume from "../documents/resume.pdf"
 
 export default function NavBar(props) {
@@ -17,15 +18,21 @@ export default function NavBar(props) {
         name: "Portfolio"
     }]
     return (
-        <div className="links">
-            {headers.map(header => {
-                let select = "null";
-                if (header.name === props.selected){
-                    select = "selected"
-                }
+        <div>
+            <div className="name">
+                <img src={logo} alt="logo" className="logo"></img>
+                <a href="/">Daniel Westphal</a>
+            </div>
+            <div className="links">
+                {headers.map(header => {
+                    let select = "null";
+                    if (header.name === props.selected) {
+                        select = "selected"
+                    }
 
-                return <a href={header.link} className={select} > {header.name} </a>
-            })}
-            <a href="#1" onClick={(e) => {/* window.open(Resume, "_blank") */ }}>Resume</a>
+                    return <a href={header.link} className={select} > {header.name} </a>
+                })}
+                <a href="#1" onClick={(e) => {/* window.open(Resume, "_blank") */ }}>Resume</a>
+            </div>
         </div>)
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,38 @@ import React from 'react'
 import '../styles/App.css'
 import '../styles/About.css'
 import NavBar from "../components/NavBar"
+import FootprintLeft from "../images/footprint_left.svg"
+import FootprintRight from "../images/footprint_right.svg"
 
 export default function About(props) {
     return (
-        <div className="About">
-            <NavBar selected="About" />
-            <h1 className='title'> My <em> life</em> at a glance</h1>
+        <div>
+            <div className="About">
+                <NavBar selected="About" />
+                <h1 className='title'> My <em> life</em> at a glance</h1>
+                <div className='footprints'>
+                    <img id="rf0" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf0" className='footprints' src={FootprintLeft} alt="right footprint" />
+                    <img id="rf1" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf1" className='footprints' src={FootprintLeft} alt="right footprint" />
+                    <img id="rf2" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf2" className='footprints' src={FootprintLeft} alt="right footprint" />
+                    <img id="rf3" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf3" className='footprints' src={FootprintLeft} alt="right footprint" />
+                    <img id="rf4" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf4" className='footprints' src={FootprintLeft} alt="right footprint" />
+                    <img id="rf5" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf5" className='footprints' src={FootprintLeft} alt="right footpring" />
+                    <img id="rf6" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf6" className='footprints' src={FootprintLeft} alt="right footpring" />
+                    <img id="rf7" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf7" className='footprints' src={FootprintLeft} alt="right footpring" />
+                    <img id="rf8" className='footprints' src={FootprintRight} alt="left footprint" />
+                    <img id="lf8" className='footprints' src={FootprintLeft} alt="right footpring" />
+                    <img id="rf9" className='footprints' src={FootprintRight} alt="left footprint" />
+                </div>
+            </div>
+
         </div>
 
     );
