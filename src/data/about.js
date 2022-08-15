2eb420596cf1cc094b3935ae48a7276ea6a88af2
Daniel Westphal <dwestphal71@gmail.com>
2022-08-15 00:55:08 -0700

    about

---
+++ src/data/about.js
@@ -0,0 +1,6 @@
+export const info = {
+    family : "I was born in New York and grew up in Connecticut with my mom, dad, younger sister (Jennifer), and dog (Maggie).",
+    education: "I'm currently finishing up my BA at Dartmouth College, where I'm double majoring in Physics and Computer Science Modified with Human Centered Design. I'm also writing an honors thesis!",
+    work: "I've had the awesome experience of interning at companies large (Amazon) and small (Tamr) as a software engineer. Currently, I'm finishing up my internship at Amazon!",
+    fun: "In my free time, I enjoy baking, hiking, and spending time with friends. I also enjoy travelling, going to museums, and playing chess."
+}
